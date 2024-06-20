1fd0edc83cfcf9571e53e8fc5edc83f1e43bd8af
Mike Lofgren <mikeylofgren@gmail.com>
2024-06-19 23:25:45 -0700

    add player object.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { scaleFactor } from './constants';
 import { k } from './kaboomCtx';
 
 k.loadSprite('spritesheet', './spritesheet.png', {
@@ -5,5 +6,43 @@ k.loadSprite('spritesheet', './spritesheet.png', {
   sliceY: 31,
   anims: {
     'idle-down': 936,
+    'walk-down': { from: 936, to: 939, loop: true, speed: 8 },
+    'idle-side': 975,
+    'walk-side': { from: 975, to: 978, loop: true, speed: 8 },
+    'idle-up': 1014,
+    'walk-up': { from: 1014, to: 1017, loop: true, speed: 8 },
   },
 });
+
+k.loadSprite('map', './map.png');
+
+k.setBackground(k.Color.fromHex('#311047'));
+
+k.scene('main', async () => {
+  const mapData = await (await fetch('./map.json')).json();
+  const layers = mapData.layers;
+
+  const map = k.make([k.sprite('map'), k.pos(0), k.scale()]);
+
+  const player = k.make([
+    k.sprite(
+      'spritesheet',
+      { anim: 'idle-down' },
+      k.area({
+        shape: new k.Rect(k.vec2(0, 3), 10, 10),
+      }),
+      k.body(),
+      k.anchor('center'),
+      k.pos(),
+      k.scale(scaleFactor),
+      {
+        speed: 250,
+        direction: 'down',
+        isInDialogue: false,
+      },
+      'player'
+    ),
+  ]);
+});
+
+k.go('main');
