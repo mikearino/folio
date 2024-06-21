ea2b00a077fee33b949e51ac6e0e9ccaabb2da73
Mike Lofgren <mikeylofgren@gmail.com>
2024-06-20 21:47:14 -0700

    updated additional information.

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my machine. I work mostly in JavaScript and have been learning TypeScript.
-    I really like React, have made a Chrome Extension that has helped our Support team immensely with efficiency. The other languages I know a bit of are Python and Ruby.
+    I really like React, have worked with Chrome extensions that have helped our Support team immensely with efficiency. The other languages I know a bit of are Python and Ruby.
     Here is my <a href="https://github.com/mikearino" target="_blank">Github</a>!`,
   cert: `This is my code school certificate from Epicodus. I hung it on the wall because I'm proud of it! This school gave me a great foundation for understanding programming.`,
   'sofa-table': `That's my couch. I like to relax here and watch YouTube. 
@@ -20,7 +20,6 @@ export const dialogueData = {
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Kaboom will soon be rebranded to Kaplay! Learn more about how to use
   Kaboom.js here <a href="https://kaboomjs.com/" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves.
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit Mike L's portfolio, just close the tab.`,
+  library: `There are a lot of programming books on my shelves. Along with books about streams, bikes and of course fiction!`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
 };
